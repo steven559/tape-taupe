eef5e505f6a5172c57116314b4801d5447f9312c
sstienface <s.stien@fondationface.org>
2018-12-03 15:04:35 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/**
+ * Created by sstienface on 03/12/2018.
+ */
+
+
+const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
+const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
+const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
